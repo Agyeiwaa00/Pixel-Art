06851d0cda960bf5a1068384114d59ca07f462a4
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-04-24 19:53:30 +0200

    test

--- src/Component/PixelArt.js
+++ src/Component/PixelArt.js
@@ -6,14 +6,18 @@ import { useState, useContext } from 'react'
         color:'lightGrey',
         setColor:() => {}
     })
+
+    // color picker function 
  function ColorPicker () {
-    const colors = ['red','blue','yellow','green','black','white','purple']
+    const colors = ['red','blue','yellow','green','black','orange','purple']
     const {setColor} = useContext(ColorContext)
     return(
-        <div>
-            <h1 style={{font:'bold'}}>Choose a color</h1>
-            {colors.map(color => <button onClick={()=> setColor(color)} key={color} style={{backgroundColour:color, width:'10px',height:'10px', margin:'10px'}} />)}
+        <div className='flex justify-center items-center'>
+            <h1 className='font-bold text-2xl m-10'>Choose a color</h1>
+            {colors.map(color => <button className='w-5 h-5 m-3'onClick={()=> setColor(color)} key={color} style={{backgroundColor:color}}/>)}
+            
         </div>
+        
     )
  }
 
@@ -24,15 +28,17 @@ import { useState, useContext } from 'react'
     return <div onClick={()=> setPixelColor(color)} style={{height:'20px', width:'20px', backgroundColor:pixelColor, margin:'1px'}}/>
  }
 
+ // function for defining the grid
  function Pixels (){
-    let pixels=[]  //arrays to hold rows & colums
-    for(let i=0; i<100; i++) pixels.push(<pixels/>) // Add each row to the rows array
+    const pixels=[]  //arrays to hold rows & colums
+    for(let i=0; i<100; i++) pixels.push(<Pixel/>) // Add each row to the rows array
 
     // Render the grid
     return(
         <div style={{display:'grid', gridTemplateColumns:'repeat(10,1fr)', width:'210px', margin:'0 auto'}}>
             {pixels}
         </div>
+        
     )
  }
 
